92542ea308c3392712d1287419385c9ff9ce28e2
kibak.yoon <kibak.yoon@samsung.com>
2015-12-02 19:31:17 +0900

    capi-sensor: Fixing build issue related to changing internal API
    Because interface of sensord_change_event_max_batch_latency API is changed,
    the code using this one has to be changed.
    
    Change-Id: Ie1e59bba30168e96636b3a19ec69190a484ab16d
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -518,7 +518,13 @@ int sensor_listener_set_max_batch_latency(sensor_listener_h listener, unsigned i
 	if (listener->magic != SENSOR_LISTENER_MAGIC)
 		return SENSOR_ERROR_INVALID_PARAMETER;
 
-	if (!sensord_change_event_max_batch_latency(listener->id, max_batch_latency))
+	int type;
+	unsigned int event_id;
+
+	type = (int)listener->type;
+	event_id = type << SENSOR_SHIFT_TYPE | 0x1;
+
+	if (!sensord_change_event_max_batch_latency(listener->id, event_id, max_batch_latency))
 		return SENSOR_ERROR_NOT_SUPPORTED;
 
 	_D("success sensor_set_max_batch_latency");
