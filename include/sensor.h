4a558ea80abb7143ef07dd6bcc0aa8333c02bc27
Mu-Woong Lee <muwoong.lee@samsung.com>
2016-02-03 14:15:56 +0900

    doxygen: add since tag for wearable profile
    Change-Id: I3aa6b707c29c2b8249bf2b828e93f1f8e5e9ce7d
    Signed-off-by: Mu-Woong Lee <muwoong.lee@samsung.com>

--- include/sensor.h
+++ include/sensor.h
@@ -130,7 +130,7 @@ typedef enum
  * @brief   Enumeration for pedestrian state.
  * @details In its #sensor_event_s, #SENSOR_HUMAN_PEDOMETER reports the user's
  *          pedestrian state as one of the followings.
- * @since_tizen 3.0
+ * @since_tizen @if MOBILE 3.0 @elseif WEARABLE 2.3.2 @endif
  */
 typedef enum
 {
@@ -145,7 +145,7 @@ typedef enum
  * @brief   Enumeration for sleep state.
  * @details In its #sensor_event_s, #SENSOR_HUMAN_SLEEP_MONITOR reports the user's
  *          sleep state as one of the followings.
- * @since_tizen 3.0
+ * @since_tizen @if MOBILE 3.0 @elseif WEARABLE 2.3.2 @endif
  */
 typedef enum
 {
@@ -188,23 +188,35 @@ typedef enum
 	                                             @if MOBILE (Since 2.4) @endif @n
 	                                             Privilege : http://tizen.org/privilege/healthinfo */
 	SENSOR_GYROSCOPE_UNCALIBRATED,          /**< Uncalibrated Gyroscope sensor
-	                                             @if MOBILE (Since 2.4) @elseif WEARABLE (Since 3.0) @endif */
+	                                             @if MOBILE (Since 2.4) @elseif WEARABLE (Since 2.3.2) @endif */
 	SENSOR_GEOMAGNETIC_UNCALIBRATED,        /**< Uncalibrated Geomagnetic sensor
-	                                             @if MOBILE (Since 2.4) @elseif WEARABLE (Since 3.0) @endif */
+	                                             @if MOBILE (Since 2.4) @elseif WEARABLE (Since 2.3.2) @endif */
 	SENSOR_GYROSCOPE_ROTATION_VECTOR,       /**< Gyroscope-based rotation vector sensor
-	                                             @if MOBILE (Since 2.4) @elseif WEARABLE (Since 3.0) @endif */
+	                                             @if MOBILE (Since 2.4) @elseif WEARABLE (Since 2.3.2) @endif */
 	SENSOR_GEOMAGNETIC_ROTATION_VECTOR,     /**< Geomagnetic-based rotation vector sensor
-	                                             @if MOBILE (Since 2.4) @elseif WEARABLE (Since 3.0) @endif */
-	SENSOR_ACTIVITY_STATIONARY = 0x100,     /**< Activity sensor detecting that the device is not moving (Since 3.0) */
-	SENSOR_ACTIVITY_WALK,                   /**< Activity sensor detecting that the device is on a user who is walking (Since 3.0) */
-	SENSOR_ACTIVITY_RUN,                    /**< Activity sensor detecting that the device is on a user who is running (Since 3.0) */
-	SENSOR_ACTIVITY_IN_VEHICLE,             /**< Activity sensor detecting that the device is in a moving vehicle, like a car (Since 3.0) */
-	SENSOR_ACTIVITY_ON_BICYCLE,             /**< Activity sensor detecting that the device is on a bicycle (Since 3.0) */
-	SENSOR_GESTURE_MOVEMENT = 0x200,        /**< Gesture sensor detecting any significant movements (Since 3.0) */
-	SENSOR_GESTURE_WRIST_UP,                /**< Gesture sensor detecting wrist-up gestures (Since 3.0) */
-	SENSOR_GESTURE_WRIST_DOWN,              /**< Gesture sensor detecting wrist-down gestures (Since 3.0) */
-	SENSOR_HUMAN_PEDOMETER = 0x300,         /**< Pedometer (Since 3.0) @n Privilege : http://tizen.org/privilege/healthinfo */
-	SENSOR_HUMAN_SLEEP_MONITOR,             /**< Sleep monitor (Since 3.0) @n Privilege : http://tizen.org/privilege/healthinfo */
+	                                             @if MOBILE (Since 2.4) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_ACTIVITY_STATIONARY = 0x100,     /**< Activity sensor detecting that the device is not moving
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_ACTIVITY_WALK,                   /**< Activity sensor detecting that the device is on a user who is walking
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_ACTIVITY_RUN,                    /**< Activity sensor detecting that the device is on a user who is running
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_ACTIVITY_IN_VEHICLE,             /**< Activity sensor detecting that the device is in a moving vehicle, like a car
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_ACTIVITY_ON_BICYCLE,             /**< Activity sensor detecting that the device is on a bicycle
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_GESTURE_MOVEMENT = 0x200,        /**< Gesture sensor detecting any significant movements
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_GESTURE_WRIST_UP,                /**< Gesture sensor detecting wrist-up gestures
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_GESTURE_WRIST_DOWN,              /**< Gesture sensor detecting wrist-down gestures
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif */
+	SENSOR_HUMAN_PEDOMETER = 0x300,         /**< Pedometer
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif
+											     @n Privilege : http://tizen.org/privilege/healthinfo */
+	SENSOR_HUMAN_SLEEP_MONITOR,             /**< Sleep monitor (Since 3.0)
+	                                             @if MOBILE (Since 3.0) @elseif WEARABLE (Since 2.3.2) @endif
+											     @n Privilege : http://tizen.org/privilege/healthinfo */
 	SENSOR_LAST,                            /**< End of sensor enum values (Deprecated since 3.0) */
 	SENSOR_CUSTOM = 0x2710,                 /**< Custom sensor (Deprecated since 3.0) */
 } sensor_type_e;
@@ -227,7 +239,7 @@ typedef enum
 
 /**
  * @brief   Enumeration for sensor listener behavior attributes
- * @since_tizen 3.0
+ * @since_tizen @if MOBILE 3.0 @elseif WEARABLE 2.3.2 @endif
  * @see     #sensor_axis_e
  * @see     #sensor_pause_e
  */
@@ -251,7 +263,7 @@ typedef enum
  *          By default, #SENSOR_AXIS_DISPLAY_ORIENTED is used.
  *          If you need to use the data that are not affected by display orientations,
  *          #SENSOR_AXIS_DEVICE_ORIENTED needs to be set.
- * @since_tizen 3.0
+ * @since_tizen @if MOBILE 3.0 @elseif WEARABLE 2.3.2 @endif
  */
 typedef enum
 {
@@ -265,7 +277,7 @@ typedef enum
  * @details To be power-efficient, you can set the policy of how to pause and resume
  *          a sensor listener regarding the system status.
  *          By default, #SENSOR_PAUSE_ALL is used to obtain the maximum power efficiency.
- * @since_tizen 3.0
+ * @since_tizen @if MOBILE 3.0 @elseif WEARABLE 2.3.2 @endif
  */
 typedef enum
 {
@@ -353,7 +365,7 @@ int sensor_get_sensor_list(sensor_type_e type, sensor_h **list, int *sensor_coun
  * @brief   Checks whether a given sensor is a wake-up sensor or not.
  * @details If a sensor is a wake-up sensor, the sensor is able to wake-up the system
  *          to report its sensor data even if the system is in sleep mode.
- * @since_tizen 3.0
+ * @since_tizen @if MOBILE 3.0 @elseif WEARABLE 2.3.2 @endif
  *
  * @param[in]   sensor  A sensor handle to check
  * @param[out]  wakeup  If the sensor is a wake-up sensor, @c true;
@@ -643,7 +655,7 @@ int sensor_listener_set_max_batch_latency(sensor_listener_h listener, unsigned i
  *          what is the reference coordinate of the sensor values,
  *          and when the system is allowed to turn off the sensor implicitly to reduce the power consumption.
  *          See #sensor_attribute_e for more details about the available control parameters.
- * @since_tizen 3.0
+ * @since_tizen @if MOBILE 3.0 @elseif WEARABLE 2.3.2 @endif
  *
  * @param[in]   listener	A listener handle
  * @param[in]   attribute	An attribute to change
