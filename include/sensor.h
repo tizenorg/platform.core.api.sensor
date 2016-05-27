ea2ca9ddda28d1ade7120e2d14ecd8be0d6ab886
kibak.yoon <kibak.yoon@samsung.com>
2016-05-27 17:07:06 +0900

    capi-system-sensor: drafts for recorder APIs and new sensor types
    Change-Id: I7d1bfd80ae832b70fc3f7444f0e310be320fdef7
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

--- include/sensor.h
+++ include/sensor.h
@@ -108,8 +108,10 @@ typedef enum
 	SENSOR_ERROR_NOT_SUPPORTED         = TIZEN_ERROR_NOT_SUPPORTED,        /**< Not supported */
 	SENSOR_ERROR_PERMISSION_DENIED     = TIZEN_ERROR_PERMISSION_DENIED,    /**< Permission denied */
 	SENSOR_ERROR_OUT_OF_MEMORY         = TIZEN_ERROR_OUT_OF_MEMORY,        /**< Out of memory */
+	SENSOR_ERROR_NO_DATA               = TIZEN_ERROR_NO_DATA,              /**< No data available */
 	SENSOR_ERROR_NOT_NEED_CALIBRATION  = TIZEN_ERROR_SENSOR | 0x03,        /**< Sensor doesn't need calibration */
 	SENSOR_ERROR_OPERATION_FAILED      = TIZEN_ERROR_SENSOR | 0x06,        /**< Operation failed */
+	SENSOR_ERROR_ALREADY_STARTED       = TIZEN_ERROR_SENSOR | 0x07,        /**< Sensor is already started */
 } sensor_error_e;
 
 
@@ -195,6 +197,18 @@ typedef enum
 	                                             @n Privilege : http://tizen.org/privilege/healthinfo */
 	SENSOR_HUMAN_SLEEP_MONITOR,             /**< Sleep monitor (Since 3.0)
 	                                             @n Privilege : http://tizen.org/privilege/healthinfo */
+	SENSOR_EXERCISE = 0x400,                /**< Exercise (Since 3.0)
+	                                             @n Privilege : http://tizen.org/privilege/healthinfo */
+	SENSOR_WEAR_STATUS = 0x1A00,            /**< Wear status (Since 3.0) */
+	SENSOR_WEAR_ON_MONITOR,                 /**< Wear on monitor (Since 3.0) */
+
+	SENSOR_GESTURE_MOVEMENT = 0x1200,       /**< Movement (Since 3.0) */
+	//SENSOR_GESTURE_WRIST_UP,                /**< Wrist up (Since 3.0) */
+
+	SENSOR_NO_MOVE_DETECTOR = 0x1A80,       /**< No move detector (Since 3.0) */
+	SENSOR_STRESS_MONITOR = 0x1A87,         /**< Stress monitor (Since 3.0) */
+	SENSOR_STAIR_TRACKER = 0x1A89,          /**< Stair tracker (Since 3.0) */
+	SENSOR_STEP_DETECTOR,                   /**< Step dectector (Since 3.0) */
 	SENSOR_LAST,                            /**< End of sensor enum values (Deprecated since 3.0) */
 	SENSOR_CUSTOM = 0x2710,                 /**< Custom sensor (Deprecated since 3.0) */
 } sensor_type_e;
@@ -264,7 +278,6 @@ typedef enum
 	SENSOR_PAUSE_ON_POWERSAVE_MODE = 2, /**< The sensor pauses while the power-save mode is enabled */
 	SENSOR_PAUSE_ALL = 3,               /**< The sensor pauses in all the above cases */
 } sensor_pause_e;
-
 
 /**
  * @brief   Checks whether a given sensor type is supported in the current device.
